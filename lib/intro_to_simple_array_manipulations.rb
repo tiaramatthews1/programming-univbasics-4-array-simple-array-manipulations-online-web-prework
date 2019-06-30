b7b15386254446cc0e99971a9b07123df323cd85
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-06-30 16:34:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,6 +42,6 @@ def using_delete(array, string)
   array.delete(string)
 end
 
-def using_delete_at(array)
-  array.delete_at(-2, 'Robocop')
+def using_delete_at(Integer)
+  array.delete_at(2)
 end
