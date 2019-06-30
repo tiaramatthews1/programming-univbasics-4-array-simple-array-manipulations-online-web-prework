17963e6640b84d57c5e8f3e711d049bdbde3865e
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-06-30 16:30:45 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,6 +42,6 @@ def using_delete(array, string)
   array.delete(string)
 end
 
-def using_delete_at(array)
-  array.delete_at(array)
+def using_delete_at(array, Integer)
+  array.delete_at('Robo')
 end
