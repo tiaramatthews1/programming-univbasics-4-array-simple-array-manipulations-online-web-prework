4b91fe5e3da4bf9fffd483af81c7e91504d20418
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-06-30 15:52:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,8 +30,8 @@ def using_insert(array, item)
   array.insert(4, 'Python')
 end
 
-def using_uniq(array, string)
-  array.uniq(string)
+def using_uniq(array)
+  array.uniq
 end
 
 def using_flatten(array, )
