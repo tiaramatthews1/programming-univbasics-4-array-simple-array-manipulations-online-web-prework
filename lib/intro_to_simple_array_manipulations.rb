a294afce0238c12bd0c8f5ce44bfd57720b2ef13
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-06-30 15:39:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,8 +26,8 @@ def using_concat(array, string)
   array.concat(string)
 end
 
-def using_insert(array)
-  array.insert()
+def using_insert(array, 'Python')
+  array.insert(4)
 end
 
 def using_uniq(array)
