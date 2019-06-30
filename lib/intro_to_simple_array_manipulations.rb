685832b6e1983b2485c50261be314594b4208ed7
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-06-30 15:33:30 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,43 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift 
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array, string)
+  array.concat(string)
+end
+
+def using_insert(array)
+  array.insert()
+end
+
+def using_uniq(array)
+  array.uniq(haircuts)
+end
+
+def using_flatten(array, string)
+  array.flatten(string)
+end
+
+def using_delete(array)
+  array.delete(array)
+end
